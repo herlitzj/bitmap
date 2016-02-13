4ff4e24740969b01b34f211c86b42644722274ff
Jeff <herlitzj@gmail.com>
2016-02-13 10:24:13 -0500

    Added more logging and new user model creation logic

--- app/passport-facebook.js
+++ app/passport-facebook.js
@@ -2,19 +2,42 @@ var User = require('./user_model.js');
 var passport = require('passport')
 var FacebookStrategy = require('passport-facebook').Strategy;
 
+
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_KEY,
     clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL: process.env.FACEBOOK_REDIRECT_URL
   },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({'provider': 'facebook', 'uid': profile.id}, 
-      function(err, user) {
-      if (err) { 
-        console.log("Passport Error: ", err);
-        return done(err); }
-      console.log("Facebook Authentication completed successfully")
-      done(null, user);
-    });
-  }
-));
+  function(req, accessToken, refreshToken, profile, done) {
+    findOrCreateUser = function(){
+      console.log("==== Facebook Authentication Started ====");
+      User.findOne({'provider': 'facebook', 'uid': profile.id}, function(err, user){
+        if (err){
+          console.log('Error in Facebook Authentication: '+err);
+          return done(err);
+        }
+        if (user) {
+          console.log('User already exists');
+          return done(null, false, req.flash('message', 'User Already Exists'));
+        } else {
+          var newUser = new User();
+          newUser.provider = 'facebook';
+          newUser.uid = profile.id;
+          newUser.accessToken = accessToken;
+          newUser.refreshToken = refreshToken;
+
+          newUser.save(function(err) {
+            if (err){
+              console.log('Error saving new user'+err);
+              throw err;
+            }
+            console.log('User Registration Successfull');
+            return done(null, newUser);
+          });
+        }
+      });
+    };
+  process.nextTick(findOrCreateUser);
+  console.log("==== Facebook Authentication Complete ====");
+  })
+);
